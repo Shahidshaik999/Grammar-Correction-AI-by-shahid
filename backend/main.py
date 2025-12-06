b00ce3f45253a56e5ef80b163e1d62916a57d291
Shaik Shahid <shahidshaik9898p@gmail.com>
2025-12-06 22:01:37 +0530

    Updated backend for Render deployment

--- backend/main.py
+++ backend/main.py
@@ -17,8 +17,8 @@ app = FastAPI()
 origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "http://localhost:8080",
-    "http://127.0.0.1:8080",
+    "https://grammar-correction-ai-by-shahid.vercel.app/",
+    "http://localhost:5173",
 ]
 
 app.add_middleware(
